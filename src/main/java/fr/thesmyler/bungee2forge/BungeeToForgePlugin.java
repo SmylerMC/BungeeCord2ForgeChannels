9c954b16f44ddc80d7b69bcfb1a5ce5d0142f55a
SmylerMC <smyler@mail.com>
2022-01-16 21:54:31 +0100

    Added javadoc to BungeeToForgePlugin.java

--- src/main/java/fr/thesmyler/bungee2forge/BungeeToForgePlugin.java
+++ src/main/java/fr/thesmyler/bungee2forge/BungeeToForgePlugin.java
@@ -4,6 +4,15 @@ import fr.thesmyler.bungee2forge.imp.ForgeChannelRegistryImplementation;
 import net.md_5.bungee.api.plugin.Plugin;
 
 /**
+ * When this is loaded as a plugin by BungeeCord, no further action is needed to use the library.
+ * However, if ou wish to shade the library in your own plugin, you will need to:
+ * <ul>
+ *  <li>Call {@link #onEnable(Plugin)} with your own plugin as the argument when it gets enabled.</li>
+ *  <li>Call {@link #onDisable(Plugin)} with your own plugin as the argument when it gets disable.</li>
+ * </ul>
+ * This will register and unregister the necessary event listeners.
+ *
+ *
  * @author SmylerMC
  *
  */
